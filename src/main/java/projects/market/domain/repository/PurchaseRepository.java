63c54926b4de4477412181fbdd59963e08ea5770
FranKR02 <frankdiego007@hotmail.com>
2023-11-18 00:18:43 -0500

    Se crearon interfaces que dan la base de los metodos que se pueden usar

---
+++ src/main/java/projects/market/domain/repository/PurchaseRepository.java
@@ -0,0 +1,13 @@
+package projects.market.domain.repository;
+
+
+import projects.market.domain.Purchase;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface PurchaseRepository {
+    List<Purchase> getAll();
+    Optional<List<Purchase>> getByClient(String clientId);
+    Purchase save(Purchase purchase);
+}
